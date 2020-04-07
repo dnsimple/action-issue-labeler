53a7242a7576822ee683a74b7971ce680b48c233
Ole Michaelis <ole.michaelis@dnsimple.com>
2020-04-07 23:28:58 +0200

    initial version

--- index.js
+++ index.js
@@ -1,22 +1,56 @@
 const core = require('@actions/core');
-const wait = require('./wait');
+const fs = require('fs');
+const yaml = require('js-yaml');
+const github = require('@actions/github');
+const regexParser = require("regex-parser");
 
 
 // most @actions toolkit packages have async methods
 async function run() {
-  try { 
-    const ms = core.getInput('milliseconds');
-    console.log(`Waiting ${ms} milliseconds ...`)
+  try {
+    const token = core.getInput('repo-token', {required: true});
+    const configPath = core.getInput('configuration-path', {required: true});
 
-    core.debug((new Date()).toTimeString())
-    await wait(parseInt(ms));
-    core.debug((new Date()).toTimeString())
+    const client = new github.GitHub(token);
 
-    core.setOutput('time', new Date().toTimeString());
-  } 
+    const config = await loadConfig(configPath)
+    core.debug(`Loaded configuration ${JSON.stringify(config)}`)
+
+    const labels = await determineLables(github.context.payload.issue.body, config)
+    core.info(`The following ${ labels.length } lables will be applied: ${ labels.join(',') }`)
+
+    if (labels.length > 0) {
+      addLabels(client, github.context.payload.issue.number, labels)
+    }
+  }
   catch (error) {
     core.setFailed(error.message);
   }
+}
+
+async function loadConfig(path) {
+  let config = yaml.safeLoad(fs.readFileSync(path, 'utf8'))
+
+  Object.keys(config).map((key) => {
+    config[key] = config[key].map((regex) => { return regexParser(regex) })
+  })
+
+  return config
+}
+
+async function determineLables(issue, config) {
+  return Object.keys(config).filter((label) => {
+    return config[label].some((regex) => { return regex.test(issue) })
+  })
+}
+
+async function addLabels(client, issueNr, labels)  {
+  await client.issues.addLabels({
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      issue_number: issueNr,
+      labels: labels
+    });
 }
 
 run()
